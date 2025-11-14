f95245903da327758a8678c998feaaa86b645dfc
Joachim Meyer <jmeyer@scitenia.org>
2025-11-13 19:18:58 -0700

    Pages - Statistics - Rework base behavior.
    Change the page to load all data for one site once selected. Then give the user a further
    option to refine by observation period. This also removes the figure with some base
    summary statistics (next commit)

--- modules/statistics.py
+++ modules/statistics.py
@@ -1,43 +1,65 @@
+import ibis
 import pandas as pd
 
-from modules.data_path import MELT_RATES_DIR
-
-def load_site_names():
-    site_names = []
-
-    for site in MELT_RATES_DIR.iterdir():
-        if site.is_dir():
-            site_names.append(site.name)
-
-    return site_names
-
-def load_dates(site_name):
-    folder_path = MELT_RATES_DIR.joinpath(site_name)
-    dates = []
-
-    for file in folder_path.iterdir():
-        if file.suffix == ".csv":
-            date = file.stem.split("_")[1]
-            dates.append(date)
-
-    return dates
+from modules.database import LOCATIONS, MELT_RATES
 
 
-def load_statistics(site_name, dates):
-    folder_path = MELT_RATES_DIR.joinpath(site_name)
-    for file in folder_path.iterdir():
-        if dates in file.stem:
-            csv_file = file
-        break
+def load_site_names() -> pd.DataFrame:
+    """
+    Load all available glacier site names.
 
-    raw_csv = pd.read_csv(csv_file)
+    :return:
+        Dataframe with 'Glacier_ID' for filtering and a 'label' for dropdown label.
+    """
+    return (
+        LOCATIONS.join(MELT_RATES, LOCATIONS.Glacier_ID == MELT_RATES.Site)
+        .select(
+            [
+                LOCATIONS.Official_name.name("label"),
+                LOCATIONS.Glacier_ID,
+            ]
+        )
+        .distinct()
+        .order_by(ibis.asc(LOCATIONS.Glacier_ID))
+    ).execute()
 
-    # Drop unwanted columns
-    unwanted_columns = [
-        'X_i', 'Y_i', 'TimeSeparation', 'VerticalAdjustment_i', 'VerticalAdjustment_f', 'Density_i', 'Density_f'
-    ]
-    figure_data = raw_csv.drop(
-        columns=[col for col in unwanted_columns if col in raw_csv.columns]
+
+def load_periods(site_name: str) -> pd.DataFrame:
+    """
+    Load available observation periods for a given site.
+
+    :param site_name: Glacier ID from the dropdown
+
+    :return:
+        Dataframe with raw to start (for filtering) and formatted start and end date
+        for dropdown label.
+    """
+    return (
+        MELT_RATES.filter(MELT_RATES.Site == site_name)
+        .mutate(
+            start_date=MELT_RATES.Date_start.strftime("%Y-%m-%d"),
+            end_date=MELT_RATES.Date_end.strftime("%Y-%m-%d"),
+        )
+        .select(MELT_RATES.Date_start, "start_date", "end_date")
+        .distinct()
+        .order_by(ibis.asc(MELT_RATES.Date_start))
+        .execute()
     )
 
-    return raw_csv, figure_data
+
+def load_statistics(site_name: str, date: str = None) -> pd.DataFrame:
+    """
+    Load statistics for a given location and optionally filter by the observed
+    time range.
+
+    :param site_name: ID selected from the dropdown
+    :param date:
+
+    :return:
+        DataFrame with raw csv data
+    """
+    query = MELT_RATES.filter(MELT_RATES.Site == site_name)
+    if date:
+        query = query.filter(MELT_RATES.Date_start == date)
+
+    return query.execute()
--- pages/Statistics-dashboard.py
+++ pages/Statistics-dashboard.py
@@ -1,65 +1,48 @@
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-from modules.statistics import load_dates, load_site_names, load_statistics
-from modules.data_path import FIGURE_EXPORT
+from modules.statistics import load_site_names, load_statistics, load_periods
 
-# Title and introductory information
-st.title('📊 Iceberg Statistics Dashboard')
-st.info('Click here for the [Fjord Abbreviation List & Paired Dates](https://docs.google.com/spreadsheets/d/1kCcKqf717kK3_Xx-GDe0f61jhlUpZ5n6BN1qtiw7S4w/edit?gid=0#gid=0)')
+st.title("📊 Iceberg Statistics Dashboard")
 
-# User interactions
 with st.container():
     st.header("Filter")
     menu_col1, menu_col2 = st.columns(2)
 
 with menu_col1:
     site_name = st.selectbox(
-        "Select Site Name:",
-        load_site_names(),
+        "Site Name:",
+        load_site_names().to_dict("records"),
+        index=None,
+        placeholder="Select a glacier site",
+        format_func=lambda x: x["label"],
     )
 
-# Construct folder and file paths
 if site_name:
-    dates = load_dates(site_name)
-
     with menu_col2:
         date_range = st.selectbox(
-            "Select Date Range:",
-            dates
+            "Observation Periods:",
+            load_periods(site_name["Glacier_ID"]).to_dict("records"),
+            index=None,
+            placeholder="Filter to an observation period",
+            format_func=lambda x: f"{x['start_date']} to {x['end_date']}",
         )
+else:
+    date_range = None
 
-if site_name and date_range:
-    csv_data, figure_data = load_statistics(site_name, date_range)
+if site_name or date_range:
+    loader_args = dict(site_name=site_name["Glacier_ID"])
+    if date_range:
+        loader_args["date"] = date_range["Date_start"]
 
-    st.write("### Iceberg Meltrate Information:")
-    st.dataframe(csv_data)
+    data = load_statistics(**loader_args)
 
-    # Download option to export CSV data
-    csv_data = csv_data.to_csv(index=False)
+    st.write("### Raw data:")
+    st.dataframe(data)
+
     st.download_button(
         label="Download .csv file",
-        data=csv_data,
+        help="Download the above shown data as a .csv file",
+        data=data.to_csv(index=False),
         file_name="iceberg_melt_rates.csv",
         mime="text/csv",
     )
-
-    # Figure
-    st.write("### Correlogram of Iceberg Features")
-    fig, ax = plt.subplots(figsize=(10, 8))
-    sns.heatmap(
-        figure_data.corr(), annot=True, fmt=".2f", cmap="coolwarm", ax=ax
-    )
-    st.pyplot(fig)
-
-    # Save figure option
-    fig.savefig(FIGURE_EXPORT)
-
-    with open(FIGURE_EXPORT, "rb") as img_file:
-        st.download_button(
-            label="Download PNG image",
-            data=img_file,
-            file_name="correlogram.png",
-            mime="image/png",
-        )

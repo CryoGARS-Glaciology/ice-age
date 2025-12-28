0fa573935a303a0212fed30f82581bbb6e528e77
Joachim Meyer <jmeyer@scitenia.org>
2025-12-28 14:05:14 -0700

    Modules - Database - Cache DB and table resources.

--- modules/database.py
+++ modules/database.py
@@ -1,7 +1,18 @@
 import ibis
+import streamlit as st
+
+
+@st.cache_resource
+def get_table(table):
+    return CONNECTION.table(table)
+
+
+@st.cache_resource
+def get_connection():
+    return ibis.duckdb.connect(DB_PATH, read_only=True, extensions=["spatial"])
 
 DB_PATH = "catalog-data/ice_age.duckdb"
-CONNECTION = ibis.duckdb.connect(DB_PATH, read_only=True, extensions=["spatial"])
+CONNECTION = get_connection()
 
-LOCATIONS = CONNECTION.table("locations")
-MELT_RATES = CONNECTION.table("meltrates")
+LOCATIONS = get_table("locations")
+MELT_RATES = get_table("meltrates")

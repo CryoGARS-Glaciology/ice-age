0291a2bf4449d4f44ef6ac689930e7b13e9dd506
Joachim Meyer <jmeyer@scitenia.org>
2026-01-01 13:20:09 -0700

    Modules - Remove unused constant for meltrates.
    All stored in DuckDB now.

--- modules/data_path.py
+++ modules/data_path.py
@@ -1,9 +1,3 @@
 # All paths to data sources are defined here
 
-from pathlib import Path
-
-# Map on Home showing all locations
-# Statistics Dashboard
-MELT_RATES_DIR = Path("catalog-data/Melt-rates")
-
 SHAPEFILE_CATALOG_DIR = "catalog-data/iceberg-shapefiles"

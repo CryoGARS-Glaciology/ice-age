4050d334796162f1b2375fda974707f073d119a4
Joachim Meyer <jmeyer@scitenia.org>
2026-03-29 19:10:54 -0600

    App setup - Update and expand the steps detailing the process for data import.
    Update the README to only set up the conda environment and use the data import page within
    the app to populate the database.

--- README.md
+++ README.md
@@ -16,16 +16,28 @@ uses the [mamba](https://mamba.readthedocs.io/en/latest/installation/mamba-insta
 package manager, which is a [drop-in replacement](https://mamba.readthedocs.io/en/latest/user_guide/mamba.html) 
 for `conda`. All the commands used with `conda` can be executed the same manner with `mamba`.
 
-ICE-AGE set-up and commands shown below are executed in a terminal.
+ICE-AGE set-up and commands shown below are executed in a terminal. There are
+two parts to the setup:
+
+1. Setting up the conda environment and run the application
+2. Importing the data using the running application
 
 ### Conda environment setup
-* Clone this repository to a local path on your machine
+
+* Clone this repository to a local path on your machine.
+  This step creates a local copy of the repository on your machine, which is
+  used to run the application.
+  
   ```bash
   cd /path/to/cloned/location
   git clone https://github.com/CryoGARS-Glaciology/ice-age.git
   ```
-* Create the `ice_age` conda environment
-  ```bash
+* Create the `ice_age` conda environment using the `environment.yml` file
+  provided in the repository.
+  This will install all the required Python packages and dependencies to run the
+  application.
+
+* ```bash
   cd /path/to/cloned/location/ice-age
   mamba env create -f environment.yml 
   ```
@@ -34,27 +46,28 @@ ICE-AGE set-up and commands shown below are executed in a terminal.
 The required data that powers this application is not included in this repository.
 To download the data, go to: TBD
 
-After downloading the and extracting the data and move the content to the root of the cloned 
-repository with the name `catalog-data`.
-```bash
-cd /path/to/dowloaded/data/
-mv catalog-data /path/to/cloned/location/ice-age/
-```
-
 ### Starting the application
-In a terminal
-* Activate the conda environment
-  ```bash
-  conda activate ice_age
-  ```
 * Navigate to the cloned repository
   ```bash
   cd /path/to/cloned/location/ice-age
   ```
-* Start the app
+* Start the application
   ```bash
-  streamlit run ice_age_app.py
+  ./start_ice_age.sh
   ```
+  When the application is successfully started using the above command, a
+  browser window will
+  open automatically. The startup page of the application will have instructions
+  for importing
+  the data downloaded in the previous step. Follow these instructions to finish
+  the setup.
+
+### Stopping the application
+
+To stop the application, simply press `CTRL + C` in the terminal where the
+application is
+running. This will stop the Streamlit server and close the application.
+
 ## Core Technologies
 * [Streamlit](https://streamlit.io/)
 * [DuckDB](https://duckdb.org/)
--- pages/Data-Import.py
+++ pages/Data-Import.py
@@ -25,32 +25,39 @@ st.header(
     divider=True,
 )
 st.markdown(
-    "This page will guide you through the process on getting the downloaded data "
-    "into the local database."
+    "This page will guide you through the process on getting the downloaded Zenodo data "
+    "into the local database to start exploring the data. The setup requires basic "
+    "knowledge of the command line."
 )
 st.header("Steps", divider=True)
 st.markdown(
     """
-    ### Download the data from [Zenodo](https://zenodo.org/)
+    ### 1. Download the data from [Zenodo](https://zenodo.org/)
     
     All data for this app is publicly available on Zenodo.
     [LINK](https://)
     
-    ### Create a folder inside the application repository
+    ### 2. Create a folder inside the application repository
     
-    On your local machine, go to the root of the cloned repository. 
+    On your local machine, go to the root location where you copied the repository from GitHub. 
     ```
     cd /path/to/ice-age
     ```
-    Create a new folder for the data inside the repository.
+    Create a new folder for the application data inside the repository. This well serve
+    as the root folder for all the data files.
     ```
     mkdir catalog-data
     ```
-    Note that the folder name needs to be `catalog-data` for the import process to work. 
+    Note that the application expects the folder name needs to be __`catalog-data`__ for the 
+    import process to work. 
     
-    ### Move the downloaded data into the new folder
+    ### 3. Move the downloaded Zenodo data into the new folder
     
-    Once finished moving the data inside this folder, your folder structure should look like this:
+    Move the downloaded Zenodo data into the new location and unpack the archive. This
+    process can use the native file explorer of your operating system.
+    
+    Once finished, your folder structure should look similar to this, with the file names
+    matching the names of the Zenodo archive:
     ```
     catalog-data/
         meltrates-csv/
@@ -62,13 +69,21 @@ st.markdown(
         images/
         Glacier-Locations.csv
     ```
-    ### Populate the database  
+    ### 4. Populate the database  
+    
     Click the button below to populate the database with the data.
     """
 )
 
-st.button(
-    "Populate DB",
-    on_click=create_db,
-    type="primary",
+if st.button("Populate DB", type="primary"):
+    with st.spinner("Populating database..."):
+        populate_db()
+
+st.markdown(
+    """   
+        ### 5. All Done!
+        
+        You are now ready to explore the data. The side navigation bar now has additional
+        entries under the __"Data"__ category.
+        """
 )
